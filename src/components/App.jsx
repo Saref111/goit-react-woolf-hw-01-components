fc59a3547fc10b2740f712a029dada78cfa251eb
Ivan Borovyk <Ivan_Borovyk@epam.com>
2024-02-03 17:22:39 +0200

    added data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,11 @@
 import { Profile } from "./Profile/Profile";
+
+import user from '../data/user.json';
+
 import './App.scss';
 
 export const App = () => {
-  const userData = {
-    "username": "Jacques Gluke",
-    "tag": "jgluke",
-    "location": "Ocho Rios, Jamaica",
-    "avatar": "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
-    "stats": {
-      "followers": 5603,
-      "views": 4827,
-      "likes": 1308
-    }
-  };
   return (
-    <Profile {...userData}/>
+    <Profile {...user}/>
   );
 };
