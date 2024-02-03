a16935ca9e8359136028d19afcd537ed5f5739f3
Ivan Borovyk <Ivan_Borovyk@epam.com>
2024-02-03 17:55:17 +0200

    added Statistics component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,18 @@
 import { Profile } from "./Profile/Profile";
+import { Statistics } from "./Statistics/Statisctis";
 
 import user from '../data/user.json';
+import stats from '../data/stats.json';
 
 import './App.scss';
 
 export const App = () => {
   return (
-    <Profile {...user}/>
-  );
+    <>
+      <Profile {...user}/>
+      <Statistics title="Upload stats" stats={stats} />
+    </>
+  )
 };
+
+
---
+++ src/components/Statistics/Statisctis.jsx
@@ -0,0 +1,20 @@
+import css from './Statistics.module.scss';
+
+export const Statistics = ({title, stats}) => {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+
+      {stats?.length && (
+        <ul className={css.statList}>
+          {stats.map(({ id, label, percentage }) => (
+            <li key={id} className={css.item}>
+              <span className={css.label}>{label}</span>
+              <span className={css.percentage}>{percentage}%</span>
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
+  );
+};
