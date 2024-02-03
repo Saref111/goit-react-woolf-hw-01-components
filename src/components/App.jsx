b13b75f3cf11250b6c1ecac1c8f17bc7e666158c
Ivan Borovyk <Ivan_Borovyk@epam.com>
2024-02-03 17:18:16 +0200

    added Profile component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import { Profile } from "./Profile/Profile";
+import './App.scss';
+
 export const App = () => {
+  const userData = {
+    "username": "Jacques Gluke",
+    "tag": "jgluke",
+    "location": "Ocho Rios, Jamaica",
+    "avatar": "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+    "stats": {
+      "followers": 5603,
+      "views": 4827,
+      "likes": 1308
+    }
+  };
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Profile {...userData}/>
   );
 };
